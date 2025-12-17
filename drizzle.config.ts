e1f82ed1f92d23275a5807af388b456a5df6d9eb
MSuiram <marius.valard@gmail.com>
2025-12-17 16:36:05 +0100

    Fix URL

--- drizzle.config.ts
+++ drizzle.config.ts
@@ -5,6 +5,6 @@ export default defineConfig({
     schema: "./db/schema.ts",
     out: "./drizzle",
     dbCredentials: {
-        url: process.env.DATABASE_URL!,
+        url: "postgresql://postgres:AZERTY1234@localhost:5432/dbtest",
     },
 });
